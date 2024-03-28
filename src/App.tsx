3eb6fb8ba6369781b0ddd4eaefe1dde184f9e27d
unknown <daria.lantyugova@mail.ru>
2024-03-28 16:38:31 +0300

    header with themeSwitcher added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { Container } from "components/Container";
+import { TheHeader } from "components/TheHeader";
+
 function App() {
   return (
-<>
-</>
+<Container>
+<TheHeader/>
+</Container>
   );
 }
 
---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Container.module.scss';
+import { ReactNode } from 'react';
+
+interface ContainerProps { 
+  children: ReactNode,
+}
+
+export const Container = ({children }: ContainerProps) => (
+  <div className={styles.container} >
+    {children}
+  </div>
+);
