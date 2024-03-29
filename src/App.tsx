e4d2d2439a64ba426aaf8f785e0aaaeb48d065ea
unknown <daria.lantyugova@mail.ru>
2024-03-29 17:03:57 +0300

    Search2 component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Container } from "components/Container";
 import { Search } from "components/Search";
+import { Search2 } from "components/Search2";
 import { TheHeader } from "components/TheHeader";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
 <Container>
 <TheHeader/>
 <Search hasError onSubmit={()=>{}}/>
+{/* <Search2 hasError onSubmit={()=>{}}/> */}
 </Container>
   );
 }
---
+++ src/components/Search2/Search2.tsx
@@ -0,0 +1,40 @@
+import styles from './Search2.module.scss';
+import { ReactComponent as SearchIcon } from 'assets/icon-search.svg';
+import { Button } from 'components/Button';
+
+interface Search2Props {
+  hasError: boolean,
+  onSubmit: (text: string)=> void,
+}
+
+type FormFields ={
+  username: HTMLInputElement,
+}
+
+export const Search2 = ({ hasError, onSubmit}: Search2Props) => {
+
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement & FormFields >)=> {
+      event.preventDefault();
+      const text = event.currentTarget.username.value;
+      if(text) {
+        onSubmit(text);
+        event.currentTarget.reset();
+      }  
+    }
+
+  return (
+    <form  onSubmit={handleSubmit}  autoComplete='off'>
+          <div className={styles.search} >
+ <label htmlFor='search' className={styles.label}>
+  <SearchIcon/>
+ </label>
+  <input type='text' className={styles.textField} id='search' name='username' placeholder='Search GitHub name...'/>
+{hasError && (<div className={styles.error}>No result</div>)}
+<Button>Search</Button>
+    </div>
+    </form>
+
+  );
+}
+
