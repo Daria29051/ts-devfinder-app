554b5ea349f409e22375790a50b59579ff122086
unknown <daria.lantyugova@mail.ru>
2024-03-29 18:03:10 +0300

    UserStat added to UserCard

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Container } from "components/Container";
 import { Search } from "components/Search";
 import { Search2 } from "components/Search2";
 import { TheHeader } from "components/TheHeader";
+import { UserCard } from "components/UserCard";
+import { defaultUser } from "mock";
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
 <TheHeader/>
 <Search hasError onSubmit={()=>{}}/>
 {/* <Search2 hasError onSubmit={()=>{}}/> */}
+<UserCard {...defaultUser}/>
 </Container>
   );
 }
---
+++ src/mock/index.ts
@@ -0,0 +1,17 @@
+import { LocalGithubUser } from "types";
+
+export const defaultUser: LocalGithubUser = {
+    "login": "wycats",
+    "id": 4,
+    "avatar": "https://avatars.githubusercontent.com/u/4?v=4",
+    "name": "Yehuda Katz",
+    "company": "@tildeio ",
+    "blog": "http://yehudakatz.com",
+    "location": "Portland, OR",
+    "bio": 'Developer',
+    "twitter": "wycats",
+    "repos": 286,
+    "followers": 10188,
+    "following": 13,
+    "created": "2008-01-12T05:38:33Z",
+  }
